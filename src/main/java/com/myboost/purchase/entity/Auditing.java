42ff0c6d54bd19583afc79a7a1c739abc4b97c02
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2025-03-11 14:17:33 +0700

    (feat:setup): add Auditing class and custom exception

---
+++ src/main/java/com/myboost/purchase/entity/Auditing.java
@@ -0,0 +1,27 @@
+package com.myboost.purchase.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Data
+public abstract class Auditing {
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", insertable = false, updatable = true)
+    private LocalDateTime deletedAt;
+}
