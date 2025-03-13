3ef2e72feffc9d14fe3818cd570e9cd639f17927
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2025-03-13 11:26:14 +0700

    (feat:order): fix error type purchase order detail

--- src/main/java/com/myboost/purchase/repository/PurchaseOrderDetailRepository.java
+++ src/main/java/com/myboost/purchase/repository/PurchaseOrderDetailRepository.java
@@ -1,5 +1,6 @@
 package com.myboost.purchase.repository;
 
+import com.myboost.purchase.entity.PurchaseOrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PurchaseOrderDetailRepository extends JpaRepository<PurchaseOrderDetailRepository, Long> {
+public interface PurchaseOrderDetailRepository extends JpaRepository<PurchaseOrderDetail, Long> {
     @Query(value = "SELECT p FROM PurchaseOrderDetail p WHERE p.deletedAt is null")
-    List<PurchaseOrderDetailRepository> findActivePurchaseOrderDetails();
+    List<PurchaseOrderDetail> findActivePurchaseOrderDetails();
 }
